9cff7b9b127a1f6e7a128001cb57fe5146548c3d
Tsaone <tsaoneb3@gmail.com>
2023-08-05 09:10:07 +0200

    Worked on react app HTML, CSS and JS

---
+++ src/Weather.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Gaborone",
+    temperature: 22,
+    date: "Saturday 16:00",
+    description: "Clear",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 33,
+    wind: 7.6,
+  };
+  return (
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city..."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+      <p className="link-source">
+        <a
+          href="https://github.com/tsaonehh/weather-react"
+          alt="My GitHub link"
+        >
+          Open-source code
+        </a>
+        , by Tsaone W Bejile
+        
+      </p>
+    </div>
+  );
+}
